6b5fa2a536628ebb5acd6f879dfdace9a26f48e6
Santiago Osorio Quintero <saostero@gmail.com>
2022-05-14 12:52:48 -0500

    Unit test of twitter controller was created

---
+++ src/__test__/twitter.controller.spec.ts
@@ -0,0 +1,37 @@
+import { ConfigModule } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import config from '../config';
+import { TwitterService } from '../services/twitter.service';
+import { TwitterModule } from '../modules/twitter.module';
+import { TwitterController } from '../controllers/twitter.controller';
+
+describe('TwitterController', () => {
+  let controller: TwitterController;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TwitterModule,
+        ConfigModule.forRoot({
+          envFilePath: '.env',
+          load: [config],
+          isGlobal: true,
+        }),
+      ],
+      providers: [TwitterService],
+    }).compile();
+
+    controller = module.get<TwitterController>(TwitterController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('getTimeline must provide array of twits', async () => {
+    const result = await controller.getTimeline();
+    console.log(typeof result[0].user.id);
+    expect(typeof result).toBe('object');
+    expect(result.length).toBeGreaterThan(0);
+    expect(typeof result[0].user.id).toBe('number');
+  });
+});
